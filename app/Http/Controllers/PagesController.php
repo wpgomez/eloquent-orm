1384e39b949934b88455338f01ceab8cd622b87f
wpgomez <wp_gomez@hotmail.com>
2016-04-21 18:37:58 -0500

    orderBy, take, get

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -6,10 +6,14 @@ use Illuminate\Http\Request;
 
 use EloquentORM\Http\Requests;
 use EloquentORM\Http\Controllers\Controller;
+use EloquentORM\User;
 
 class PagesController extends Controller
 {
     public function home(){
-        return view('pages.home');
+        $users = User::orderBy('id','DESC')
+            ->take(10)
+            ->get();
+        return view('pages.home', compact('users'));
     }
 }
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -8,5 +8,23 @@
         </div>
     </div>
 
-    Pagina del Home
+    <div class="container">
+        <h1 class="page-header">
+            Ultimos Usuarios Registrados
+        </h1>
+        <table class="table table-hover table-striped">
+            <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nombre</th>
+                <th>Email</th>
+                <th>Genero</th>
+                <th>Biografia</th>
+            </tr>
+            </thead>
+            <tbody>
+            @include('partials.list-users')
+            </tbody>
+        </table>
+    </div>
 @stop
