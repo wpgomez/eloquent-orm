c2fd433c0b657ef18fca2f616b7295984ebd5766
wpgomez <wp_gomez@hotmail.com>
2016-04-22 09:42:32 -0500

    Delete, Form

---
+++ resources/views/partials/head-users.blade.php
@@ -0,0 +1,10 @@
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>Nombre</th>
+        <th>Email</th>
+        <th>Genero</th>
+        <th>Biografia</th>
+        <th>&nbsp;</th>
+    </tr>
+</thead>
